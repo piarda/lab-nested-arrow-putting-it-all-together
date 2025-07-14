f837c0bd64596067ef8acc452008bc4329814657
piarda <matt.rindini@gmail.com>
2025-07-14 14:54:07 -0400

    Defined outer function, implemented arrow function

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+// Outer function will initialize the login feature.
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  // Variable to keep track of the numer of login attempts.
 
+  // Arrow function to hangle the login attempts.
+  const attemptLogin = (passwordAttempt) => {
+    attemptCount++;
+
+    // Checks to see if the password attempts exceed 3 attempts.
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    // Successful and failed logins depending on if password matches.
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+
+  return attemptLogin;
+  // Returns the inner function.
+}
 
 
 module.exports = {
